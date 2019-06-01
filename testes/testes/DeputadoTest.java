e69775a0858d644714be33d22ec3ad31f7455978
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-06-01 10:18:36 -0300

    JUnit Testes Classe Deputado Concluido.

---
+++ testes/testes/DeputadoTest.java
@@ -0,0 +1,64 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import entities.Deputado;
+
+class DeputadoTest {
+
+	@Test
+	void testConstrutorDataDeInicioNula() {
+		try {
+			Deputado d = new Deputado(null);
+			fail("");
+		} catch (NullPointerException iae) {
+		}
+	}
+	
+	@Test
+	void testConstrutorDataDeInicioVazia() {
+		try {
+			Deputado d = new Deputado("");
+			fail("");
+		} catch (NullPointerException iae) {
+		}
+	}
+
+	@Test
+	void testConstrutorDataDeInicioApenasEspacos() {
+		try {
+			Deputado d = new Deputado("");
+			fail("");
+		} catch (NullPointerException iae) {
+		}
+	}
+	
+	@Test
+	void testConstrutorDataDeInicioInvalida() {
+		try {
+			Deputado d = new Deputado("31022001");
+			fail("");
+		} catch (IllegalArgumentException iae) {
+		}
+	}
+	
+	@Test
+	void testConstrutorDataDeInicioFutura() {
+		try {
+			Deputado d = new Deputado("01013001");
+			fail("");
+		} catch (IllegalArgumentException iae) {
+		}
+	}
+	
+	
+	
+	@Test
+	void testGetDataDeInicio() {
+		Deputado d = new Deputado("01012001");
+		assertEquals("01/01/2001", d.getDataDeInicio());
+	}
+
+}
