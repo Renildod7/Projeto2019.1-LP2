66cde714d8637e51dd6d9c0f53ac9f050237237d
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-06-01 10:10:46 -0300

    JUnit Testes PessoaController Concluido.

--- testes/testes/PessoaControllerTest.java
+++ testes/testes/PessoaControllerTest.java
@@ -112,6 +112,162 @@ class PessoaControllerTest {
 
 	
 	
+	@Test
+	void testCadastraDeputadoDniNulo() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado(null, "01012001");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
 	
+	@Test
+	void testCadastraDeputadoDniVazio() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("", "01012001");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDniApenasEspacos() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("    ", "01012001");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDniInvalido() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("A00000000-0", "01012001");
+			fail("");
+		} catch (IllegalArgumentException npe) {
+		}
+	}
+	
+	
+	
+	@Test
+	void testCadastraDeputadoDataDeInicioNula() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", null);
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDataDeInicioVazia() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", "");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDataDeInicioApenasEspacos() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", "   ");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDataDeInicioDataInvalida() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", "31022001");
+			fail("");
+		} catch (IllegalArgumentException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoDataDeInicioDataFutura() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", "01013005");
+			fail("");
+		} catch (IllegalArgumentException npe) {
+		}
+	}
+	
+	@Test
+	void testCadastraDeputadoPessoaNaoEncontrada() {
+		try {
+			this.cntrl.cadastrarDeputado("000000000-0", "01012001");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	
+	
+	@Test
+	void testExibePessoaDniNulo() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.exibirPessoa(null);
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testExibePessoaDniVazio() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.exibirPessoa("");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testExibePessoaDniApenasEspacos() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.exibirPessoa("   ");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testExibePessoaDniInvalido() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		try {
+			this.cntrl.exibirPessoa("A00000000-0");
+			fail("");
+		} catch (IllegalArgumentException npe) {
+		}
+	}
+	
+	@Test
+	void testExibePessoaDniPessoaNaoEncontrada() {
+		try {
+			this.cntrl.exibirPessoa("000000000-0");
+			fail("");
+		} catch (NullPointerException npe) {
+		}
+	}
+	
+	@Test
+	void testExibePessoaPessoaCadastrada() {
+		this.cntrl.cadastrarPessoa("nome", "000000000-0", "XX", "interesses", "YY");
+		assertEquals("nome - 000000000-0 (XX) - YY - Interesses: interesses", this.cntrl.exibirPessoa("000000000-0"));
+	}
 
 }
